44c4c83363504a48a7de3dee34d93b0edda72c22
David Clarke <david26th@gmail.com>
2022-09-06 19:02:43 +0100

    Corrected position of For Each Loop

--- src/com/DCDoncaster/calcengine/Main.java
+++ src/com/DCDoncaster/calcengine/Main.java
@@ -9,11 +9,6 @@ public class Main {
         char[] opCodes = {'+', '-', '*', '/'};
         double results[] = new double[opCodes.length];
 
-
-//        double value1 = 100.0d;
-//        double value2 = 10.0d;
-//        double result = 0.0;
-//        char opCode = '/';
         for (int i = 0; i < opCodes.length; i++) {
             switch (opCodes[i]) {
                 case '+':
@@ -33,30 +28,11 @@ public class Main {
                     results[i] = 0.0d;
                     break;
             }
-//            System.out.println(results);
-        for(double currentResult:results)
-            System.out.println(currentResult);
+
 
         }
+
+        for(double currentResult:results)
+            System.out.println(currentResult);
     }
 }
-
-
-
-//        if (opCode == '+')
-//            result = value1 + value2;
-//        else if (opCode == '-')
-//            result = value1 - value2;
-//        else if (opCode == '*')
-//            result = value1 * value2;
-//        else if (opCode == '/') {
-//            if (value2 != 0)
-//                result = value1 / value2;
-//        }
-//            else {
-//                System.out.println("Invalid opCode: " + opCode);
-//                result = 0.0d;
-//            }
-//        System.out.println(result);
-//    }
-//    }
