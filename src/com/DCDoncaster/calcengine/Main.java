5cc754e9512d262fad3f0ff00a07a6231d1dbb37
David Clarke <david26th@gmail.com>
2022-09-06 17:38:13 +0100

    Initial Commit with Arrays

--- src/com/DCDoncaster/calcengine/Main.java
+++ src/com/DCDoncaster/calcengine/Main.java
@@ -3,31 +3,41 @@ package com.DCDoncaster.calcengine;
 public class Main {
 
     public static void main(String[] args) {
-        double value1 = 100.0d;
-        double value2 = 10.0d;
-        double result = 0.0;
-        char opCode = '/';
 
-        switch (opCode) {
-            case '+':
-                result = value1 + value2;
-                break;
-            case '-':
-                result = value1 - value2;
-                break;
-            case '*':
-                result = value1 * value2;
-                break;
-            case '/':
-                result = value2 != 0 ? value1 / value2: 0;
-                break;
-            default:
-                System.out.println("Invalid opCode" + opCode);
-                result = 0.0d;
-                break;
+        double[] leftVals = {100.0d, 25.0d, 5.0d, 124.0d};
+        double[] rightVals = {2.0d, 22.0d, 7.0d, 62.0d};
+        char[] opCodes = {'+', '-', '*', '/'};
+        double results[] = new double[opCodes.length];
+
+
+//        double value1 = 100.0d;
+//        double value2 = 10.0d;
+//        double result = 0.0;
+//        char opCode = '/';
+        for (int i = 0; i < opCodes.length; i++) {
+            switch (opCodes[i]) {
+                case '+':
+                    results[i] = leftVals[i] + rightVals[i];
+                    break;
+                case '-':
+                    results[i] = leftVals[i] - rightVals[i];
+                    break;
+                case '*':
+                    results[i] = leftVals[i] * rightVals[i];
+                    break;
+                case '/':
+                    results[i] = rightVals[i] != 0 ? leftVals[i] / rightVals[i] : 0;
+                    break;
+                default:
+                    System.out.println("Invalid opCode" + opCodes[i]);
+                    results[i] = 0.0d;
+                    break;
+            }
+//            System.out.println(results);
+        for(double currentResult:results)
+            System.out.println(currentResult);
+
         }
-        System.out.println(result);
-
     }
 }
 
